ef0a365afd8c64ef3e51d4ead316e2987ff7cd6e
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-11-25 11:03:37 +0800

    chore: playground store type

--- stores/playground.ts
+++ stores/playground.ts
@@ -3,19 +3,9 @@ import type { File } from '../structures/File'
 
 export type PlaygroundStatus = 'init' | 'mount' | 'install' | 'start' | 'ready' | 'error'
 
-export interface PlaygroundState {
-  files: File[]
-  status: PlaygroundStatus
-  error: { message: string } | undefined
-  stream: ReadableStream | undefined
-  previewUrl: string
-  previewLocation: {
-    origin: string
-    fullPath: string
-  }
-}
+export type PlaygroundState = ReturnType<typeof usePlaygroundStore>
 
-export const usePlaygroundStore = defineStore<'playground', PlaygroundState>('playground', () => {
+export const usePlaygroundStore = defineStore('playground', () => {
   const status = ref<PlaygroundStatus>('init')
   const error = shallowRef<{ message: string }>()
   const stream = ref<ReadableStream | undefined>()
@@ -26,13 +16,12 @@ export const usePlaygroundStore = defineStore<'playground', PlaygroundState>('pl
   })
   const previewUrl = computed(() => previewLocation.value.origin + previewLocation.value.fullPath)
 
-  return reactive({
+  return {
     files: shallowRef<File[]>([]),
     status,
     error,
     stream,
     previewUrl,
     previewLocation,
-  })
-  // TODO: find a way to type this
-}) as unknown as () => PlaygroundState
+  }
+})
